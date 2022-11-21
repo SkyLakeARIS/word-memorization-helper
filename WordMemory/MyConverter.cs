651f08c0a3398c1559a087d36cf3cb6aa076b7c2
SkyLake <bunkete15@gmail.com>
2022-11-22 02:09:47 +0900

    일부 데이터 형태의 경우 생성한 hex 데이터가 파싱 과정에서 문제가 생기는 부분 개선
    메모가 기록되지 않은 경우같이 구분자(문자)가 붙어 파싱시 빈문자("")가 생겨 문제가 생기는 문제.
    따라서 뜻, 메모 데이터의 경우 마지막 데이터에는 구분자를 추가하지 않도록 수정.

--- WordMemory/MyConverter.cs
+++ WordMemory/MyConverter.cs
@@ -3,34 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WordMemory.Data;
 
 namespace WordMemory
 {
     public static class MyConverter
     {
-        private static StringBuilder StringBuilder;
+        private static StringBuilder stringBuilder = null;
 
         public static void Initialize()
         {
-            StringBuilder = new StringBuilder(1024);
+	        stringBuilder = new StringBuilder(2048);
         }
         public static void Release()
         {
-            StringBuilder = null;
+	        stringBuilder.Clear();
+            stringBuilder = null;
         }
+
+        public static string WordDataToHexString(WordData wordToConvert)
+        {
+            // 내용물 제거.
+	        stringBuilder.Clear();
+
+	        // 1. 해시 변환
+            stringBuilder.Append(Int32ToHexX4(wordToConvert.Hash));
+            stringBuilder.Append(DataParser.DATA_SEPARATOR);
+
+            // 2. 암기 여부 변환
+            stringBuilder.Append(Int32ToHexX4((Int32)wordToConvert.RememberType));
+            stringBuilder.Append(DataParser.DATA_SEPARATOR);
+
+            // 3. 단어 이름 변환
+            stringBuilder.Append(StringToHex(wordToConvert.WordName));
+            stringBuilder.Append(DataParser.DATA_SEPARATOR);
+
+            // 4. 뜻 변환
+            for (Int32 index = 0; index < wordToConvert.MeanList.Count; ++index)
+            {
+                stringBuilder.Append(StringToHex(wordToConvert.MeanList[index]));
+                // 마지막 데이터는 데이터 분리문자를 붙이지 않음. (파싱 과정에서 에러 발생)
+                if (index < wordToConvert.MeanList.Count - 1)
+	            {
+		            stringBuilder.Append(DataParser.MEAN_DATA_SEPARATOR);
+	            }
+            }
+
+            // 5. 메모 변환
+            if (!(string.IsNullOrWhiteSpace(wordToConvert.Memo) || string.Empty == wordToConvert.Memo))
+            {
+	            stringBuilder.Append(DataParser.DATA_SEPARATOR);
+	            stringBuilder.Append(StringToHex(wordToConvert.Memo));
+            }
+
+            return stringBuilder.ToString();
+        }
+
         public static string HexToString(string hexString)
         {
-            StringBuilder.Clear();
+	        stringBuilder.Clear();
 
             hexString = hexString.Trim();
             string[] splited = hexString.Split(' ');
             foreach(string hex in splited)
             {
                 int hexInt = Convert.ToInt32(hex, 16);
-                StringBuilder.Append(char.ConvertFromUtf32(hexInt));
+                stringBuilder.Append(char.ConvertFromUtf32(hexInt));
             }
             
-            return StringBuilder.ToString();
+            return stringBuilder.ToString();
         }
 
         public static Int32 HexToInt32(string hexString)
@@ -55,17 +96,21 @@ namespace WordMemory
         {
             return intToConvert.ToString("X4");
         }
+
         // 코드 중복 제거
         private static string stringToHex(string stringToConvert)
         {
-            StringBuilder.Clear();
+	        stringBuilder.Clear();
 
             foreach (char ch in stringToConvert)
             {
-                StringBuilder.Append(Convert.ToString(ch, 16));
-                StringBuilder.Append(' ');
+	            stringBuilder.Append(Convert.ToString(ch, 16));
+	            stringBuilder.Append(' ');
             }
-            return StringBuilder.ToString();
+
+            string hexString = stringBuilder.ToString();
+            
+            return stringBuilder.ToString();
         }
     }
 }
