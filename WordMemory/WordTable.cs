a56c9db42f11c2ef0071c5aacb5a03dcbdff1b0d
SkyLake <bunkete15@gmail.com>
2022-11-22 02:13:54 +0900

    데이터 검색시 발생하는 문제 해결.
    검색시 잘못된 가정으로 인한 문제 해결.(리스트가 존재할 것이라고 생각했으나 실제는 null)

--- WordMemory/WordTable.cs
+++ WordMemory/WordTable.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,88 +13,103 @@ namespace WordMemory
     {
 	    // 백과사전도 아니고 개인 학습 목적의 프로그램에 이정도면 충분하다 못해 넘치는 크기일 것.
         private static List<WordData>[] wordHashTable;
-	    private static readonly Int32 tableSize = 5101;
+	    private static readonly Int32 TABLE_SIZE = 5101;
+	    private static readonly Int32 BUCKET_SIZE_DEFAULT = 4;
 
 
-	    public static void Initialize()
+        public static void Initialize()
 	    {
-			wordHashTable = new List<WordData>[tableSize];
+			wordHashTable = new List<WordData>[TABLE_SIZE];
 	    }
 	    public static void Release()
 	    {
 		    foreach (List<WordData> word in wordHashTable)
 		    {
-				word.Clear();
-		    }
+			    if (word != null)
+			    {
+				    word.Clear();
+			    }
+            }
 		    wordHashTable = null;
 	    }
 
 	    public static bool InsertWordData(WordData wordData)
 	    {
-		    if (IsExistUsingWordData(wordData))
+		    if (ExistsUsingWordData(wordData))
 		    {
 			    // 이미 존재하는 단어로 추가할 필요 없음.
 			    return false;
 		    }
-
-		    wordHashTable[wordData.Hash].Add(wordData);
+		    if (wordHashTable[wordData.Hash] == null)
+		    {
+			    wordHashTable[wordData.Hash] = new List<WordData>(BUCKET_SIZE_DEFAULT);
+		    }
+            wordHashTable[wordData.Hash].Add(wordData);
 		    return true;
 	    }
 
-        public static bool InsertWordData(string wordName, List<string> meanList, string memo, out Int32 outHash)
+        public static bool InsertWordData(string wordName, List<string> meanList, string memo, out WordData wordData)
 	    {
-		    if (IsExist(wordName))
+		    if (Exists(wordName))
 		    {
-				// 이미 존재하는 단어로 추가할 필요 없음.
-				outHash = -1;
-			    return false;
+                // 이미 존재하는 단어로 추가할 필요 없음.
+                wordData = null;
+                return false;
 		    }
 		    Int32 hash = calcHashdjb2(wordName);
 		    WordData newData = new WordData(hash, wordName, meanList, memo);
-
+		    if (wordHashTable[hash] == null)
+		    {
+			    wordHashTable[hash] = new List<WordData>(BUCKET_SIZE_DEFAULT);
+		    }
             wordHashTable[hash].Add(newData);
-            outHash = hash;
+            wordData = newData;
+            newData = null;
             return true;
 	    }
 
-	    public static bool UpdateWordData(WordData wordData)
-	    {
-		    if (!IsExistUsingWordData(wordData))
-		    {
-			    // 존재하지 않는 데이터는 에러.
-				Debug.Assert(false, "존재하지 않는 데이터가 업데이트 시도되었습니다. 새 데이터는 Insert입니다.");
-			    return false;
-		    }
+	   // public static bool UpdateWordData(WordData wordData)
+	   // {
+		  //  if (!ExistsUsingWordData(wordData))
+		  //  {
+			 //   // 존재하지 않는 데이터는 에러.
+				//Debug.Assert(false, "존재하지 않는 데이터가 업데이트 시도되었습니다. 새 데이터는 Insert입니다.");
+			 //   return false;
+		  //  }
 
-		    List<WordData> wordList = wordHashTable[wordData.Hash];
-		    for (Int32 index = 0; index < wordList.Count; ++index)
-		    {
-			    if (wordList[index].GetWordName().Equals(wordData.GetWordName()))
-			    {
-					wordList[index] = wordData;
-					wordData = null;
-					return true;
-			    }
-		    }
+		  //  List<WordData> wordList = wordHashTable[wordData.Hash];
+		  //  if (wordList == null)
+		  //  {
+			 //   return false;
+		  //  }
+		  //  for (Int32 index = 0; index < wordList.Count; ++index)
+		  //  {
+			 //   if (wordList[index].WordName.Equals(wordData.WordName))
+			 //   {
+				//	wordList[index] = wordData;
+				//	wordData = null;
+				//	return true;
+			 //   }
+		  //  }
 
-		    Debug.Assert(false, "이상 동작, 같은 단어를 찾지 못했습니다.");
+		  //  Debug.Assert(false, "이상 동작, 같은 단어를 찾지 못했습니다.");
 
-            return false;
-	    }
+    //        return false;
+	   // }
 
 	    public static bool FindWordData(string wordName, out WordData foundData)
 	    {
 		    foundData = null;
 		    Int32 hash = calcHashdjb2(wordName);
 		    List<WordData> data = wordHashTable[hash];
-		    if (data.Count <= 0)
+		    if (data == null || data.Count <= 0)
 		    {
 			    return false;
 		    }
 
 		    foreach (WordData word in data)
 		    {
-			    if (word.GetWordName().Equals(wordName))
+			    if (word.WordName.Equals(wordName))
 			    {
 				    foundData = word;
 				    return true;
@@ -106,7 +122,7 @@ namespace WordMemory
 	    public static bool RemoveWordData(WordData wordData)
 	    {
 		    List<WordData> data = wordHashTable[wordData.Hash];
-		    if (data.Count <= 0)
+		    if (data == null || data.Count <= 0)
 		    {
 			    return false;
 		    }
@@ -115,7 +131,7 @@ namespace WordMemory
 
 		    for (Int32 index = 0; index < data.Count; ++index)
 		    {
-			    if (data[index].GetWordName().Equals(wordData.GetWordName()))
+			    if (data[index].WordName.Equals(wordData.WordName))
 			    {
 				    data.RemoveAt(index);
 				    return true;
@@ -125,18 +141,18 @@ namespace WordMemory
 		    return false;
 	    }
 
-        public static bool IsExist(string wordName)
+        public static bool Exists(string wordName)
 	    {
 		    Int32 hash = calcHashdjb2(wordName);
 			List<WordData> data = wordHashTable[hash];
-			if (data.Count <= 0)
+			if (data == null || data.Count <= 0)
 			{
 				return false;
 			}
 
 			foreach (WordData word in data)
 			{
-				if (word.GetWordName().Equals(wordName))
+				if (word.WordName.Equals(wordName))
 				{
 					return true;
 				}
@@ -145,17 +161,17 @@ namespace WordMemory
 		    return false;
 	    }
 
-        public static bool IsExistUsingWordData(WordData wordData)
+        public static bool ExistsUsingWordData(WordData wordData)
         {
 	        List<WordData> data = wordHashTable[wordData.Hash];
-	        if (data.Count <= 0)
+	        if (data == null || data.Count <= 0)
 	        {
 		        return false;
 	        }
 
-	        foreach (WordData word in data)
+            foreach (WordData word in data)
 	        {
-		        if (word.GetWordName().Equals(wordData.GetWordName()))
+		        if (word.WordName.Equals(wordData.WordName))
 		        {
 			        return true;
 		        }
@@ -172,7 +188,7 @@ namespace WordMemory
 			    hash = ((hash << 5) + hash) + ch; /* hash * 33 + c */
 		    }
 
-		    return hash/tableSize;
+		    return hash%TABLE_SIZE;
 	    }
     }
 }
