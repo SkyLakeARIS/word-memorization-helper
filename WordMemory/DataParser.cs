5984f09f0cc265c236619455c1312262b0593a98
SkyLake <bunkete15@gmail.com>
2022-11-22 02:10:30 +0900

    환경설정 파일 구조 수정에 따른 코드 수정
    환경설정 파일 구조 수정에 따른 코드 수정

--- WordMemory/DataParser.cs
+++ WordMemory/DataParser.cs
@@ -10,16 +10,16 @@ namespace WordMemory
 {
     public static class DataParser
     {
-        private static readonly char dataSeparator = '#';
-        private static readonly char meanDataSeparator = '\n';
-        private static readonly char wordSeparator = '@';
+        public static readonly char DATA_SEPARATOR = '#';
+        public static readonly char MEAN_DATA_SEPARATOR = '\n';
+        public static readonly char WORD_SEPARATOR = '@';
 
         public static bool ParseData(string wordData, out WordData word)
         {
             Debug.Assert((!string.IsNullOrWhiteSpace(wordData) || wordData != ""), "wordName은 단어 이름이어야 합니다.");
 
             // 마지막에 붙는 #가 오른쪽을 공백으로 넣어버릴지 의문.
-            string[] splitedData = wordData.Split(dataSeparator);
+            string[] splitedData = wordData.Split(DATA_SEPARATOR);
 
             Debug.Assert((splitedData.Length == 4 || splitedData.Length == 5), "개체를 만드는데 필요한 데이터가 모자랍니다. 파일을 확인하세요.");
             if(splitedData.Length < 4)
@@ -31,19 +31,20 @@ namespace WordMemory
             Int32 hash = MyConverter.HexToInt32(splitedData[0]);
 
             // 2. 암기 여부 추출 % 열거형으로 변환 필요.
-            eRememberType rememberType = (eRememberType)MyConverter.HexToInt32(splitedData[1]);
+            ERememberType rememberType = (ERememberType)MyConverter.HexToInt32(splitedData[1]);
 
             // 3. 단어 이름 추출
             string wordName = MyConverter.HexToString(splitedData[2]);
 
             // 4. 단어 뜻 추출
-            string[] SplitedMean = splitedData[3].Split(meanDataSeparator);
+            string[] SplitedMean = splitedData[3].Split(MEAN_DATA_SEPARATOR);
             Debug.Assert(SplitedMean.Length >= 1, "개체를 만드는데 필요한 데이터가 모자랍니다.(뜻이 최소 하나 이상이어야 합니다.) 파일을 확인하세요.");
             if(SplitedMean.Length <= 0)
             {
                 word = null;
                 return false;
             }
+
             List<string> meanList = new List<string>(16);
 
             for(int count = 0; count < SplitedMean.Length; ++count)
@@ -79,7 +80,7 @@ namespace WordMemory
             Debug.Assert((!string.IsNullOrWhiteSpace(importedData) || importedData != ""), "importedData에 데이터가 없습니다.");
 
             // 1. 단어 데이터들 분리.
-            string[] splitedWordData = importedData.Split(wordSeparator);
+            string[] splitedWordData = importedData.Split(WORD_SEPARATOR);
             Debug.Assert(splitedWordData.Length > 0, "임포트할 데이터가 없습니다.");
             if (splitedWordData.Length <= 0)
             {
@@ -102,20 +103,17 @@ namespace WordMemory
         {
 	        Debug.Assert((!string.IsNullOrWhiteSpace(settingData) || settingData != ""), "settingData에 데이터가 없습니다.");
 
-            string[] splitedData = settingData.Split(dataSeparator);
-            Debug.Assert(splitedData.Length == 4, $"데이터에 이상이 있습니다. 데이터 구조 개수는 4개여야 하는데 {splitedData.Length}개 입니다.");
+            string[] splitedData = settingData.Split(DATA_SEPARATOR);
+            Debug.Assert(splitedData.Length == 3, $"데이터에 이상이 있습니다. 데이터 구조 개수는 3개여야 하는데 {splitedData.Length}개 입니다.");
 
             // 1. 보기모드 데이터 추출
-            Setting.ViewMode = (eViewMode)MyConverter.HexToInt32(splitedData[0]);
+            Setting.ViewMode = (EViewMode)MyConverter.HexToInt32(splitedData[0]);
 
             // 2. 갱신 모드 데이터 추출
-            Setting.RefreshMode = (eRefreshMode) MyConverter.HexToInt32(splitedData[1]);
+            Setting.RefreshMode = (ERefreshMode) MyConverter.HexToInt32(splitedData[1]);
 
             // 3. 타이머 설정 값 추출
             Setting.RefreshTime = MyConverter.HexToInt32(splitedData[2]);
-
-            // 4. (저장된)등록된 단어 수 추출
-            Setting.SetWordCount(MyConverter.HexToInt32(splitedData[3]));
         }
     }
 }
