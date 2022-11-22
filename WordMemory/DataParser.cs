d09b1364e876bc9fc948ff1bf5462c391c864ddb
SkyLake <bunkete15@gmail.com>
2022-11-22 20:41:34 +0900

    hash 데이터 변경 (int32 -> uint32)
    1. 일부 데이터가 해시값이 음수가 나오므로 변경함.
    2. 변경한 해시 자료형에 따라서 converter, parser에도 uint32에 대응하는 기능 제작 및 수정.
    3. 그 외 일부 로직(assert) 코드 수정 및 추가

--- WordMemory/DataParser.cs
+++ WordMemory/DataParser.cs
@@ -1,34 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 using WordMemory.Data;
 
 namespace WordMemory
 {
+	/*
+	 *  hex string을 지정한 용도에 맞게 읽어서 변환하는 클래스입니다.
+	 */
     public static class DataParser
     {
         public static readonly char DATA_SEPARATOR = '#';
         public static readonly char MEAN_DATA_SEPARATOR = '\n';
         public static readonly char WORD_SEPARATOR = '@';
 
+        /*
+		 *  넘겨받은 hex string을 단어 데이터로 변환하여 반환합니다.
+         *  성공하면 단어 데이터를 out 및 true 반환 그렇지 않으면 null out 및 false 반환.
+		 */
         public static bool ParseData(string wordData, out WordData word)
         {
             Debug.Assert((!string.IsNullOrWhiteSpace(wordData) || wordData != ""), "wordName은 단어 이름이어야 합니다.");
+
+            word = null;
+            if (string.IsNullOrWhiteSpace(wordData) || wordData == "")
+            {
+	            word = null;
+	            return false;
+            }
 
             // 마지막에 붙는 #가 오른쪽을 공백으로 넣어버릴지 의문.
             string[] splitedData = wordData.Split(DATA_SEPARATOR);
 
             Debug.Assert((splitedData.Length == 4 || splitedData.Length == 5), "개체를 만드는데 필요한 데이터가 모자랍니다. 파일을 확인하세요.");
-            if(splitedData.Length < 4)
+            if(splitedData.Length != 4 && splitedData.Length != 5)
             {
-                word = null;
                 return false;
             }
             // 1. 해시 값 추출
-            Int32 hash = MyConverter.HexToInt32(splitedData[0]);
+            UInt32 hash = MyConverter.HexToUInt32(splitedData[0]);
 
             // 2. 암기 여부 추출 % 열거형으로 변환 필요.
             ERememberType rememberType = (ERememberType)MyConverter.HexToInt32(splitedData[1]);
@@ -41,7 +51,6 @@ namespace WordMemory
             Debug.Assert(SplitedMean.Length >= 1, "개체를 만드는데 필요한 데이터가 모자랍니다.(뜻이 최소 하나 이상이어야 합니다.) 파일을 확인하세요.");
             if(SplitedMean.Length <= 0)
             {
-                word = null;
                 return false;
             }
 
@@ -60,24 +69,43 @@ namespace WordMemory
             }
 
             // 6. 개체 조립 후 반환
-            word = new WordData(hash, wordName, meanList, rememberType, memo);
+            word = new WordData((UInt32)hash, wordName, meanList, rememberType, memo);
             return true;
         }
 
-        public static void ParseDataFromFile(string wordNameToLoad, out WordData word)
-        {
-            Debug.Assert((!string.IsNullOrWhiteSpace(wordNameToLoad) || wordNameToLoad != ""), "wordName은 단어 이름이어야 합니다.");
-            string wordDataString = string.Empty;
-            FileManager.LoadWordDataFromFile(wordNameToLoad, out wordDataString);
-            if (!ParseData(wordDataString, out word))
-            {
-	            Debug.Assert(false, "데이터를 파싱하는데 문제가 발생했습니다. ParseDataFromFile");
-            }
-        }
+        /*
+		 *  지정한 단어 데이터 이름을 통해 바로 파일에서 읽어와 단어 데이터로 변환합니다.
+         *  성공하면 단어 데이터 out 그렇지 않으면 null out.
+         *  사용 보류.
+		 */
+        //public static void ParseDataFromFile(string wordNameToLoad, out WordData word)
+        //{
+        //    Debug.Assert((!string.IsNullOrWhiteSpace(wordNameToLoad) || wordNameToLoad != ""), "wordName은 단어 이름이어야 합니다.");
+        //    word = null;
+        //    if (string.IsNullOrWhiteSpace(wordNameToLoad) || wordNameToLoad != "")
+        //    {
+        //     return;
+        //    }
 
+        //    string wordDataString = string.Empty;
+        //    FileManager.LoadWordDataFromFile(wordNameToLoad, out wordDataString);
+        //    if (!ParseData(wordDataString, out word))
+        //    {
+        //     Debug.Assert(false, "데이터를 파싱하는데 문제가 발생했습니다. ParseDataFromFile");
+        //    }
+        //}
+
+        /*
+		 *  전달받은 hex string을 통해 단어 데이터들을 변환하여 리스트로 반환합니다.
+         *  성공한 데이터들은 리스트에 추가되어 반환됩니다.
+		 */
         public static void ParseImportData(string importedData, ref List<WordData> outWordDataList)
         {
             Debug.Assert((!string.IsNullOrWhiteSpace(importedData) || importedData != ""), "importedData에 데이터가 없습니다.");
+            if (string.IsNullOrWhiteSpace(importedData) || importedData == "")
+            {
+	            return;
+            }
 
             // 1. 단어 데이터들 분리.
             string[] splitedWordData = importedData.Split(WORD_SEPARATOR);
@@ -99,12 +127,23 @@ namespace WordMemory
             wordData = null;
         }
 
+        /*
+		 *  전달받은 hex string을 통해 프로그램 환경설정으로 변환해 설정값을 세팅합니다.
+		 */
         public static void ParseProgramSettingDataAndSet(string settingData)
         {
 	        Debug.Assert((!string.IsNullOrWhiteSpace(settingData) || settingData != ""), "settingData에 데이터가 없습니다.");
+	        if (string.IsNullOrWhiteSpace(settingData) || settingData == "")
+	        {
+		        return;
+	        }
 
             string[] splitedData = settingData.Split(DATA_SEPARATOR);
             Debug.Assert(splitedData.Length == 3, $"데이터에 이상이 있습니다. 데이터 구조 개수는 3개여야 하는데 {splitedData.Length}개 입니다.");
+            if (splitedData.Length != 3)
+            {
+	            return;
+            }
 
             // 1. 보기모드 데이터 추출
             Setting.ViewMode = (EViewMode)MyConverter.HexToInt32(splitedData[0]);
@@ -113,7 +152,7 @@ namespace WordMemory
             Setting.RefreshMode = (ERefreshMode) MyConverter.HexToInt32(splitedData[1]);
 
             // 3. 타이머 설정 값 추출
-            Setting.RefreshTime = MyConverter.HexToInt32(splitedData[2]);
+            Setting.RefreshTimeMilliseconds = MyConverter.HexToInt32(splitedData[2]);
         }
     }
 }
--- WordMemory/WordTable.cs
+++ WordMemory/WordTable.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WordMemory.Data;
 
 namespace WordMemory
 {
+	/*
+	 *  단어 데이터의 해시 테이블 클래스
+	 */
     public static class WordTable
     {
 	    // 백과사전도 아니고 개인 학습 목적의 프로그램에 이정도면 충분하다 못해 넘치는 크기일 것.
@@ -16,11 +14,17 @@ namespace WordMemory
 	    private static readonly Int32 TABLE_SIZE = 5101;
 	    private static readonly Int32 BUCKET_SIZE_DEFAULT = 4;
 
-
-        public static void Initialize()
+		/*
+		 *  클래스를 초기화 합니다.
+		 */
+	    public static void Initialize()
 	    {
 			wordHashTable = new List<WordData>[TABLE_SIZE];
 	    }
+
+		/*
+		 *  클래스 데이터를 해제합니다.
+		 */
 	    public static void Release()
 	    {
 		    foreach (List<WordData> word in wordHashTable)
@@ -33,13 +37,18 @@ namespace WordMemory
 		    wordHashTable = null;
 	    }
 
-	    public static bool InsertWordData(WordData wordData)
+	    /*
+		 *  해시 테이블에 이미 만들어진 단어 데이터를 삽입합니다.
+	     *  주로, 데이터 임포트, 저장된 단어 데이터를 추가시 사용.
+		 */
+        public static bool InsertWordData(WordData wordData)
 	    {
 		    if (ExistsUsingWordData(wordData))
 		    {
 			    // 이미 존재하는 단어로 추가할 필요 없음.
 			    return false;
 		    }
+			// 처음 할당되는 경우 리스트를 생성합니다. (기본 버켓 사이즈4)
 		    if (wordHashTable[wordData.Hash] == null)
 		    {
 			    wordHashTable[wordData.Hash] = new List<WordData>(BUCKET_SIZE_DEFAULT);
@@ -48,6 +57,10 @@ namespace WordMemory
 		    return true;
 	    }
 
+        /*
+		 *  해시 테이블에 단어 데이터를 생성하여 삽입합니다.
+         *  주로 단어 데이터를 생성할 때 사용.
+		 */
         public static bool InsertWordData(string wordName, List<string> meanList, string memo, out WordData wordData)
 	    {
 		    if (Exists(wordName))
@@ -56,51 +69,30 @@ namespace WordMemory
                 wordData = null;
                 return false;
 		    }
-		    Int32 hash = calcHashdjb2(wordName);
-		    WordData newData = new WordData(hash, wordName, meanList, memo);
+		    UInt32 hash = calcHashdjb2(wordName);
+			// 데이터 생성
+		    WordData newData = new WordData((UInt32)hash, wordName, meanList, memo);
+
 		    if (wordHashTable[hash] == null)
 		    {
 			    wordHashTable[hash] = new List<WordData>(BUCKET_SIZE_DEFAULT);
 		    }
+
             wordHashTable[hash].Add(newData);
+
             wordData = newData;
             newData = null;
             return true;
 	    }
 
-	   // public static bool UpdateWordData(WordData wordData)
-	   // {
-		  //  if (!ExistsUsingWordData(wordData))
-		  //  {
-			 //   // 존재하지 않는 데이터는 에러.
-				//Debug.Assert(false, "존재하지 않는 데이터가 업데이트 시도되었습니다. 새 데이터는 Insert입니다.");
-			 //   return false;
-		  //  }
-
-		  //  List<WordData> wordList = wordHashTable[wordData.Hash];
-		  //  if (wordList == null)
-		  //  {
-			 //   return false;
-		  //  }
-		  //  for (Int32 index = 0; index < wordList.Count; ++index)
-		  //  {
-			 //   if (wordList[index].WordName.Equals(wordData.WordName))
-			 //   {
-				//	wordList[index] = wordData;
-				//	wordData = null;
-				//	return true;
-			 //   }
-		  //  }
-
-		  //  Debug.Assert(false, "이상 동작, 같은 단어를 찾지 못했습니다.");
-
-    //        return false;
-	   // }
-
-	    public static bool FindWordData(string wordName, out WordData foundData)
+        /*
+		 *  단어 이름으로 해시 테이블을 검색합니다.
+         *  찾으면 단어 데이터를 반환하고 그렇지 않으면 null을 반환합니다.
+		 */
+        public static bool FindWordDataOrNull(string wordName, out WordData foundData)
 	    {
 		    foundData = null;
-		    Int32 hash = calcHashdjb2(wordName);
+		    UInt32 hash = calcHashdjb2(wordName);
 		    List<WordData> data = wordHashTable[hash];
 		    if (data == null || data.Count <= 0)
 		    {
@@ -119,7 +111,10 @@ namespace WordMemory
 		    return false;
         }
 
-	    public static bool RemoveWordData(WordData wordData)
+        /*
+		 *  해시 테이블에서 단어 데이터를 제거합니다.
+		 */
+        public static bool RemoveWordData(WordData wordData)
 	    {
 		    List<WordData> data = wordHashTable[wordData.Hash];
 		    if (data == null || data.Count <= 0)
@@ -127,10 +122,9 @@ namespace WordMemory
 			    return false;
 		    }
 
-			// 추후 wordData를 비교 함수를 오버라이드 해서 단어 명으로 비교할 수 있도록 수정.
-
 		    for (Int32 index = 0; index < data.Count; ++index)
 		    {
+				// 확인해보니 내부적으로 같은 주소인지, 문자열이 같은지 검사해 줌(특이하게)
 			    if (data[index].WordName.Equals(wordData.WordName))
 			    {
 				    data.RemoveAt(index);
@@ -141,9 +135,13 @@ namespace WordMemory
 		    return false;
 	    }
 
+        /*
+		 *  단어 이름으로 해시 테이블에 데이터가 존재하는지 확인합니다.
+         *  찾으면 true, 그렇지 않으면 false.
+		 */
         public static bool Exists(string wordName)
 	    {
-		    Int32 hash = calcHashdjb2(wordName);
+		    UInt32 hash = calcHashdjb2(wordName);
 			List<WordData> data = wordHashTable[hash];
 			if (data == null || data.Count <= 0)
 			{
@@ -161,6 +159,10 @@ namespace WordMemory
 		    return false;
 	    }
 
+        /*
+		 *  단어 데이터로 해시 테이블에 데이터가 존재하는지 확인합니다.
+         *  찾으면 true, 그렇지 않으면 false.
+		 */
         public static bool ExistsUsingWordData(WordData wordData)
         {
 	        List<WordData> data = wordHashTable[wordData.Hash];
@@ -179,16 +181,21 @@ namespace WordMemory
 
 	        return false;
         }
-        private static Int32 calcHashdjb2(string wordName)
+
+        /*
+		 *  해시를 생성하는 해시함수입니다.
+         *  단어 이름으로 해시를 생성합니다.
+		 */
+        private static UInt32 calcHashdjb2(string wordName)
 	    {
-		    Int32 hash = 5381;
+		    UInt32 hash = 5381;
 
 		    foreach (char ch in wordName)
 		    {
 			    hash = ((hash << 5) + hash) + ch; /* hash * 33 + c */
 		    }
 
-		    return hash%TABLE_SIZE;
+		    return hash%(UInt32)TABLE_SIZE;
 	    }
     }
 }
