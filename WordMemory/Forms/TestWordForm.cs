ade8d831c56d4aade6ee3f92e752b663542eba2e
SkyLake <bunkete15@gmail.com>
2022-11-22 20:45:35 +0900

    테스트 기능 구현.
    1. 뜻 테스트 기능 구현.
    2. 단어 테스트 기능 구현.

--- WordMemory/Forms/TestWordForm.cs
+++ WordMemory/Forms/TestWordForm.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using WordMemory.Data;
 
 namespace WordMemory
 {
     public partial class TestWordForm : Form
     {
+	    private WordData mWordData;
+	    private string mStartWord;
+
         public TestWordForm()
         {
             InitializeComponent();
@@ -19,20 +17,54 @@ namespace WordMemory
 
         private void TestWordForm_Load(object sender, EventArgs e)
         {
+	        ColumnHeader header1 = new ColumnHeader();
+	        header1.Text = "";
+	        header1.Name = "Mean";
+	        header1.Width = WordManager.MEAN_STRING_LENGTH_LIMIT;
+            
+            MeanListView.View = View.Details;
+	        MeanListView.Columns.Add(header1);
+	        MeanListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+	        MessageBox.Text = "뜻에 맞는 단어를 입력합니다..\n 체점 기준은 등록된 단어와 '정확한' 단어를 입력해야 합니다.";
+	        MessageBox.ForeColor = System.Drawing.Color.Red;
 
             MeanListView.Items.Clear();
+
+			updataWordData();
         }
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-            if(string.IsNullOrEmpty(Word.Text) || string.IsNullOrWhiteSpace(Word.Text))
+            if(string.IsNullOrWhiteSpace(Word.Text) || Word.Text == string.Empty)
             {
                 MessageBox.Text = "단어를 입력해 주세요.";
                 MessageBox.ForeColor = System.Drawing.Color.Red;
                 return;
             }
 
+            if (Word.Text.Length >= WordManager.WORD_STRING_LENGTH_LIMIT)
+            {
+	            MessageBox.Text = $"{WordManager.WORD_STRING_LENGTH_LIMIT}자 이내로 입력해 주세요.";
+	            MessageBox.ForeColor = System.Drawing.Color.Red;
+	            return;
+            }
+
+            if (mWordData.WordName.Equals(Word.Text))
+            {
+	            MessageBox.Text = "정답입니다.";
+	            MessageBox.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+	            MessageBox.Text = "오답입니다. 미암기로 분류됩니다.";
+	            MessageBox.ForeColor = System.Drawing.Color.Red;
+                mWordData.UpdateRememberType(ERememberType.NOT_REMEMBER);
+            }
+
             Word.Text = string.Empty;
+            mWordData = null;
+            updataWordData();
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -47,6 +79,37 @@ namespace WordMemory
             {
                 btnSubmit_Click(sender, e);
             }
+        }
+
+        private void updataWordData()
+        {
+	        // 암기된 단어리스트에서 데이터를 하나 가져옵니다.
+	        mWordData = WordManager.GetWodDataOrNullBy(ERememberType.REMEMBER);
+
+	        // 추가된 데이터가 없을 경우. (혹은 표시할 데이터가 없을 경우.)
+	        if (mWordData == null)
+	        {
+		        // 더이상 데이터가 없습니다.
+		        MessageBox.Text = "암기된 데이터가 없습니다.";
+		        MessageBox.ForeColor = Color.Red;
+		        return;
+	        }
+
+	        if (mWordData.WordName.Equals(mStartWord))
+	        {
+		        MessageBox.Text = "더이상 테스트할 데이터가 없습니다.";
+		        MessageBox.ForeColor = Color.Red;
+		        return;
+	        }
+
+	        // UI로 데이터 갱신
+	        MeanListView.BeginUpdate();
+	        MeanListView.Items.Clear();
+	        foreach (string mean in mWordData.MeanList)
+	        {
+		        MeanListView.Items.Add(mean);
+	        }
+	        MeanListView.EndUpdate();
         }
     }
 }
