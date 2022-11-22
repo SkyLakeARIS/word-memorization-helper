29323b249706eeb99d774d805a9920ab5d99923a
SkyLake <bunkete15@gmail.com>
2022-11-22 20:37:39 +0900

    UI와 상호작용 작업 등 구현 및 수정작업
    1. 종료시 release 과정에서 모든 데이터를 다시 저장하여 수정 사항을 반영하도록 하는 기능 구현.
    2. UI에서 단어 데이터를 받아서 표시할 수 있도록 단어를 골라 반환하는 함수 구현.

--- WordMemory/WordManager.cs
+++ WordMemory/WordManager.cs
@@ -1,45 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WordMemory.Data;
 using WordMemory.Forms;
 
 namespace WordMemory
 {
+	/*
+	 *  단어 데이터를 관리하는 클래스
+	 *  주로 UI에서 받은 명령을 통해 다른 클래스들과 상호작용합니다.
+	 */
     public static class WordManager
     {
 	    public static readonly Int32 MEAN_COUNT_LIMIT = 16;
 	    public static readonly Int32 MEAN_STRING_LENGTH_LIMIT = 16;
 
 	    // 사전에 등록된 단어중 가장 긴 단어가 45자.
-	    // 다만, 현실적으로 외울 수 있는 범위의 단어를 등록하는 것이
-	    // 좋을 수 있기 때문에 나중에 수정 가능성 있음.
+	    // 현실적으로 외울 수 있는 범위의 단어 길이는 아니기 때문에 넘치는 길이.
 	    public static readonly Int32 WORD_STRING_LENGTH_LIMIT = 46;
 	    public static readonly Int32 MEMO_STRING_LENGTH_LIMIT = 64;
 
         // UI에 단어를 보여주기 위해서 미/암기 데이터 리스트로 저장.
-        // 데이터를 통채로 들고있기는 해시 테이블의 의미가 많이 사라지니, 해시값으로만.
-        private static List<Int32> rememberList;
-	    private static List<Int32> notRememberList;
+        // -> 해시 값을 가지면 중복된 해시에 대해서 처리가 까다로우므로, 단어명을 들고 있는 것으로 변경.
+        private static List<string> rememberList;
+	    private static List<string> notRememberList;
+		// 메인 폼, 테스트 폼에서 단어 데이터를 요청할 때 사용하기 위해 인덱스를 저장.
+	    private static Int32 currentRememberListIndex;
+		private static Int32 currentNotRememberListIndex;
 
+	    // 좀 더 보류 후 삭제 여부 결정.
         private static Int32 numOfWord;
 
+		// 임포트시 중복 데이터에 대한 처리 옵션.
         private static EImportOption importOption;
-        // 해시가 겹치는 애들은 따로 카운트로 관리하던가?
-        // 아니면 다른 방안을 더 생각해보기.
 
+        /*
+		 *  클래스 데이터를 초기화합니다.
+		 */
         public static void Initialize()
 	    {
-		    rememberList = new List<Int32>(2048);
-		    notRememberList = new List<Int32>(2048);
+		    rememberList = new List<string>(2048);
+		    notRememberList = new List<string>(2048);
 
-            // 파일을 로드해서 해시테이블과 리스트를 동시에 초기화.
+		    // 파일을 로드해서 해시테이블과 리스트를 동시에 초기화.
             if (FileManager.DataFileNameList.Count <= 0)
 			{
 				return;
@@ -61,45 +66,106 @@ namespace WordMemory
 
 					if (wordData.RememberType == ERememberType.REMEMBER)
 					{
-						rememberList.Add(wordData.Hash);
+						rememberList.Add(wordData.WordName);
 					}
 					else
 					{
-						notRememberList.Add(wordData.Hash);
+						notRememberList.Add(wordData.WordName);
 					}
 
 					++numOfWord;
-
 				}
 			}
+
+			// 랜덤으로 시작 인덱스를 결정합니다.
+			// 단순히 매 실행마다 같은 순서를 가지는 것을 약간 피하기 위함.
+		    Random rand = new Random();
+		    currentRememberListIndex = rand.Next(rememberList.Count);
+		    currentNotRememberListIndex = rand.Next(notRememberList.Count);
+
 	    }
 
-	    public static void Release()
+        /*
+		 *  클래스 데이터를 해제합니다.
+		 */
+        public static void Release()
 	    {
-			rememberList.Clear();
+		    saveWordData();
+
+            rememberList.Clear();
 			notRememberList.Clear();
-			// 해시 클리어.
 	    }
 
-	    public static void SetImportOption(EImportOption newOption)
+        /*
+		 *  setter, 데이터 임포트 과정에서 중복 데이터에 대한 처리 옵션 설정입니다.
+		 */
+        public static void SetImportOption(EImportOption newOption)
 	    {
 		    importOption = newOption;
 	    }
 
-        public static void ShowWordToUI() // get으로 바꿔야 할 듯. UI에서 받아가도록.
-	    {
-			
-	    }
+        /*
+		 *  ERememberType으로 지정한 열거형에 맞는 리스트에서
+         *  데이터를 해시테이블에서 찾아 반환합니다.
+         *  주로 테스트, 메인 폼에서 사용.
+		 */
+        public static WordData GetWodDataOrNullBy(ERememberType typeToGet)
+        {
+	        WordData returnData = null;
 
-	    public static bool AddWordData(string wordName, List<string> meanList, string memo)
+	        switch (typeToGet)
+		    {
+				// 미암기된 단어 리스트에서 반환.
+				case ERememberType.NOT_REMEMBER:
+					if (notRememberList.Count <= 0)
+					{
+						return returnData;
+					}
+
+                    WordTable.FindWordDataOrNull(notRememberList[currentNotRememberListIndex++], out returnData);
+					if (currentNotRememberListIndex >= notRememberList.Count)
+					{
+						currentNotRememberListIndex = 0;
+					}
+					break;
+				// 암기된 단어 리스트에서 반환.
+				case ERememberType.REMEMBER:
+					if (rememberList.Count <= 0)
+					{
+						return returnData;
+					}
+                    WordTable.FindWordDataOrNull(rememberList[currentRememberListIndex++], out returnData);
+					if (currentRememberListIndex >= rememberList.Count)
+					{
+						currentRememberListIndex = 0;
+					}
+                    break;
+				default:
+					Debug.Assert(false, $"잘못된 ERememberType 데이터가 넘어왔습니다. {typeToGet}");
+					break;
+		    }
+
+		    return returnData;
+
+        }
+
+        /*
+		 *  AddWordForm에서 입력한 데이터를 통해서 단어 데이터를 생성합니다.
+		 */
+        public static bool AddWordData(string wordName, List<string> meanList, string memo)
 	    {
 		    WordData wordData = null;
 		    if (WordTable.InsertWordData(wordName, meanList, memo, out wordData))
 		    {
-			    notRememberList.Add(wordData.Hash);
+				// 처음 추가하는 단어는 무조건 암기되지 않았다고 가정합니다.
+			    notRememberList.Add(wordData.WordName);
+
+				// 바로 저장합니다.
                 string dataString = MyConverter.WordDataToHexString(wordData);
+
 				FileManager.SaveWordDataToFile(wordName, dataString);
 			    FileManager.DataFileNameList.Add(wordName);
+
                 ++numOfWord;
 
 			    wordData = null;
@@ -109,17 +175,21 @@ namespace WordMemory
 		    return false;
 	    }
 
-	    public static bool RemoveWordData(WordData wordData)
+        /*
+		 *  단어 데이터를 통해서 단어를 제거합니다.
+         *  (제거는 단어 추가가 전제이기 때문에, 단어 데이터를 인자로 받음.)
+		 */
+        public static bool RemoveWordData(WordData wordData)
 	    {
 		    if (WordTable.RemoveWordData(wordData))
 		    {
 			    if (wordData.RememberType == ERememberType.REMEMBER)
 			    {
-				    rememberList.Remove(wordData.Hash);
+				    rememberList.Remove(wordData.WordName);
 			    }
 			    else
 			    {
-				    notRememberList.Remove(wordData.Hash);
+				    notRememberList.Remove(wordData.WordName);
 			    }
 			    --numOfWord;
 				FileManager.RemoveDataFile(wordData.WordName);
@@ -130,12 +200,19 @@ namespace WordMemory
 		    return false;
 	    }
 
-	    public static bool FindWordData(string wordName, out WordData wordData)
+        /*
+		 *  단어 이름으로 해시 테이블에서 검색하여 결과를 반환합니다.
+         *  찾으면 단어 데이터 out 및 true 반환, 그렇지 않으면 null out 및 false 반환.
+		 */
+        public static bool FindWordDataOrNull(string wordName, out WordData wordData)
 	    {
-		    return WordTable.FindWordData(wordName, out wordData);
+		    return WordTable.FindWordDataOrNull(wordName, out wordData);
 	    }
 
-	    public static void UpdateWordData(WordData wordData)
+        /*
+         *  내용이 수정된 단어 데이터를 파일로 내보냅니다.
+         */
+        public static void UpdateWordData(WordData wordData)
 	    {
             //return WordTable.UpdateWordData(wordData);
             string dataString = MyConverter.WordDataToHexString(wordData);
@@ -143,16 +220,20 @@ namespace WordMemory
             FileManager.SaveWordDataToFile(wordData.WordName, dataString);
 	    }
 
+        /*
+		 *  프로그램에 등록된 모든 단어 데이터를 내보내기하기 위해
+         *  데이터를 생성하여 지정 위치에 저장합니다.
+		 */
         public static void ExportWordData()
         {
 	        WordData word = null;
             StringBuilder exportHexString = new StringBuilder(2048);
             for (Int32 countFile = 0; countFile < FileManager.DataFileNameList.Count; ++countFile)
             {
-		        if (FindWordData(FileManager.DataFileNameList[countFile], out word))
+		        if (FindWordDataOrNull(FileManager.DataFileNameList[countFile], out word))
 		        {
 					// 1. 해시 변환
-			        exportHexString.Append(MyConverter.Int32ToHexX4(word.Hash));
+			        exportHexString.Append(MyConverter.UInt32ToHexX4(word.Hash));
                     exportHexString.Append(DataParser.DATA_SEPARATOR);
 
                     // 2. 암기 여부 변환
@@ -194,12 +275,17 @@ namespace WordMemory
 	        FileManager.SaveExportData(exportHexString.ToString());
         }
 
-	    public static void ImportWordData()
+        /*
+		 * 외부에서 내보내기한 데이터를 불러와 프로그램에 등록합니다.
+         * 자동으로 저장됩니다.
+		 */
+        public static void ImportWordData()
 	    {
 		    string importedDataString = string.Empty;
 			List<WordData> wordDataList = new List<WordData>(1024);
 			if (!FileManager.LoadImportDataOrEmpty(out importedDataString))
 			{
+				Debug.Assert(false, "데이터 임포트 실패");
 				return;
 			}
 
@@ -208,6 +294,7 @@ namespace WordMemory
 
             if (wordDataList.Count <= 0)
             {
+	            Debug.Assert(false, "단어 데이터 파싱 실패");
 	            return;
             }
 
@@ -222,36 +309,41 @@ namespace WordMemory
                 if(WordTable.ExistsUsingWordData(wordDataList[wordCount]))
                 {
 					CheckImportDataSetting checkImportDataSetting = new CheckImportDataSetting();
-					
-					checkImportDataSetting.ShowDialog();
+
+					checkImportDataSetting.WordName = wordDataList[wordCount].WordName;
+
+                    checkImportDataSetting.ShowDialog();
 
 					switch (importOption)
 					{
 					case EImportOption.USE_ORIGINAL_DATA:
 						// do nothing
-						break;
+						goto NEXT_LOOP;
 					case EImportOption.USE_IMPORTED_DATA:
-						if (WordTable.FindWordData(wordDataList[wordCount].WordName, out foundDataOriginal))
+						if (WordTable.FindWordDataOrNull(wordDataList[wordCount].WordName, out foundDataOriginal))
 						{
 							foundDataOriginal.UpdateMeanData(wordDataList[wordCount].MeanList);
 							foundDataOriginal.UpdateMemo(wordDataList[wordCount].Memo);
 							foundDataOriginal.UpdateRememberType(wordDataList[wordCount].RememberType);
-							foundDataOriginal = null;
                         }
 
 						break;
 					case EImportOption.USE_MERGED_DATA:
 
-						if (WordTable.FindWordData(wordDataList[wordCount].WordName, out foundDataOriginal))
+						if (WordTable.FindWordDataOrNull(wordDataList[wordCount].WordName, out foundDataOriginal))
 						{
 							mergeWordData(ref foundDataOriginal, wordDataList[wordCount]);
-							foundDataOriginal = null;
 						}
                         break;
 					default:
 						Debug.Assert(false, $"EImportOption 값이 잘못 전달되었습니다. {(Int32)importOption}");
 						break;
 					}
+
+					// 작업한 데이터를 저장합니다.
+					string dataString = MyConverter.WordDataToHexString(foundDataOriginal);
+					FileManager.SaveWordDataToFile(foundDataOriginal.WordName, dataString);
+
                 }
                 else // 새로운 데이터 추가
                 {
@@ -260,11 +352,15 @@ namespace WordMemory
 					AddWordData(wordDataList[wordCount].WordName, wordDataList[wordCount].MeanList, wordDataList[wordCount].Memo);
 
                 }
-				// for end
+
+                NEXT_LOOP:
+                foundDataOriginal = null;
+                // for end
             }
-			// if end
-				
-	    }
+            // if end
+
+        }
+
 
 		// 임포트된 데이터를 기존 데이터에 추가합니다.
 	    // 뜻은 최대 16개까지 추가되고, 메모는 '<imported>'접두를 붙여 덧붙입니다.
@@ -314,5 +410,40 @@ namespace WordMemory
 
 			originalData.UpdateMemo($"{originalData.Memo} <imported>{importedData.Memo}");
 	    }
+
+	    /*
+	     *  프로그램에 등록된 모든 단어 데이터들을 파일로 내보냅니다.
+		 *  프로그램을 종료하기전에 실행합니다.
+		 */
+        private static void saveWordData()
+	    {
+		    WordData wordData = null;
+		    string wordDataString = string.Empty;
+		    foreach (string wordName in FileManager.DataFileNameList)
+		    {
+			    if (WordTable.FindWordDataOrNull(wordName, out wordData))
+			    {
+				    wordDataString = MyConverter.WordDataToHexString(wordData);
+				    FileManager.SaveWordDataToFile(wordName, wordDataString);
+			    }
+		    }
+	    }
+
+        // 추후에 오토 세이브 기능시 더 제대로 사용될 듯.
+        //private static void saveWordDataIfModified()
+        //{
+        //	WordData wordData = null;
+        //	foreach (string wordName in FileManager.DataFileNameList)
+        //	{
+        //		if (WordTable.FindWordDataOrNull(wordName, out wordData))
+        //		{
+        //			if (wordData.IsModified)
+        //			{
+        //				string wordDataString = MyConverter.WordDataToHexString(wordData);
+        //				FileManager.SaveWordDataToFile(wordName, wordDataString);
+        //			}
+        //		}
+        //	}
+        //}
     }
 }
